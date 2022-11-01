93916d73da65e8a929518752b17000029f8e5560
JGASmits <jossmits_3@hotmail.com>
2022-11-01 16:46:04 +0100

    add_documentation

--- R/ANANSE_seurat_wrapper.R
+++ R/ANANSE_seurat_wrapper.R
@@ -1,11 +1,12 @@
 #' export_CPM_scANANSE
 #'
 #' This functions exports CPM values from a seurat object
-#' @param seurat object
-#' @param min_cells: minimum of cells a cluster needs to be exported
-#' @param output_dir: directory where the files are outputted
-#' @param RNA_count_assay: assay of the seurat object containing the RNA count data
-#' @param cluster_id: ID used for finding clusters of cells
+#' @param seurat_object the seurat object used to export the CPM values from
+#' @param min_cells minimum of cells a cluster needs to be exported
+#' @param output_dir directory where the files are outputted
+#' @param RNA_count_assay assay of the seurat object containing the RNA count data
+#' @param cluster_id ID used for finding clusters of cells
+#' @return CPM and count files
 #' @export
 export_CPM_scANANSE <- function(seurat_object,
                                    min_cells = 50,
@@ -16,9 +17,9 @@ export_CPM_scANANSE <- function(seurat_object,
   dir.create(file.path(output_dir),showWarnings = FALSE)
   Idents(seurat_object) <- cluster_id
   print('calculating CPM')
-  seurat_object <- NormalizeData(seurat_object, 
-                                 assay  = RNA_count_assay, 
-                                 normalization.method = 'RC', 
+  seurat_object <- NormalizeData(seurat_object,
+                                 assay  = RNA_count_assay,
+                                 normalization.method = 'RC',
                                  scale.factor = 1e6
   )
   rna_count_lists = list()
@@ -32,13 +33,13 @@ export_CPM_scANANSE <- function(seurat_object,
     if (n_cells > min_cells){
       print(paste0('gather data from ', cluster, ' with ', n_cells, ' cells'))
       cluster_names[i] <- cluster
-      
+
       #lets grab the scRNAseq counts
       mat_counts <- seurat_object_cluster@assays[[RNA_count_assay]]@counts
       mat_counts <- as.matrix(rowSums(as.matrix(mat_counts)))
       colnames(mat_counts) <- cluster
       rna_count_lists[i] <- as.data.frame(mat_counts)
-      
+
       #lets also grab the normalized intensities, wich we will
       #use as a FPKM like matrix, since the reads are UMI normalized
       #we do not expect a gene-length bias
@@ -49,7 +50,7 @@ export_CPM_scANANSE <- function(seurat_object,
       i = i + 1
     }else{print(paste0(cluster,' less than ',min_cells,' skipping'))}
   }
-  
+
   #RNA count matrix
   count_matrix <- do.call(rbind, rna_count_lists)
   count_matrix <- as.data.frame(count_matrix)
@@ -58,7 +59,7 @@ export_CPM_scANANSE <- function(seurat_object,
   rownames(count_matrix) <- rownames(mat_counts)
   count_matrix <- as.data.frame(count_matrix)
   count_matrix$average <- round(rowMeans(count_matrix))
-  
+
   #FPKM matrix
   FPKM_matrix <- do.call(rbind, FPKM_count_lists)
   FPKM_matrix <- as.data.frame(FPKM_matrix)
@@ -67,10 +68,10 @@ export_CPM_scANANSE <- function(seurat_object,
   rownames(FPKM_matrix) <- rownames(mat_norm)
   FPKM_matrix <- as.data.frame(FPKM_matrix)
   FPKM_matrix$average <- rowMeans(FPKM_matrix)
-  
+
   count_file <- paste(output_dir, "RNA_Counts.tsv", sep = '/')
   CPM_file <-paste(output_dir, "TPM.tsv", sep = '/')
-  
+
   write.table(count_matrix, count_file, sep = '\t', quote = F)
   write.table(FPKM_matrix, CPM_file, sep = '\t', quote = F)
   return('done exporting cluster data files')
@@ -78,11 +79,11 @@ export_CPM_scANANSE <- function(seurat_object,
 #' export_ATAC_scANANSE
 #'
 #' This functions exports ATAC values from a seurat object
-#' @param seurat object
-#' @param min_cells: minimum of cells a cluster needs to be exported
-#' @param output_dir: directory where the files are outputted
-#' @param ATAC_peak_assay: assay of the seurat object containing the peaks and peakcounts
-#' @param cluster_id: ID used for finding clusters of cells
+#' @param seurat_object object
+#' @param min_cells minimum of cells a cluster needs to be exported
+#' @param output_dir directory where the files are outputted
+#' @param ATAC_peak_assay assay of the seurat object containing the peaks and peakcounts
+#' @param cluster_id ID used for finding clusters of cells
 #' @export
 export_ATAC_scANANSE <- function(seurat_object,
                                    min_cells = 50,
@@ -92,7 +93,7 @@ export_ATAC_scANANSE <- function(seurat_object,
 ) {
   dir.create(file.path(output_dir),showWarnings = FALSE)
   Idents(seurat_object) <- cluster_id
-  
+
   peak_count_lists = list()
   cluster_names = list()
   i = 1
@@ -103,7 +104,7 @@ export_ATAC_scANANSE <- function(seurat_object,
     if (n_cells > min_cells){
       print(paste0('gather data from ', cluster, ' with ', n_cells, ' cells'))
       cluster_names[i] <- cluster
-    
+
       #lets grab the ATAC signal
       mat <- seurat_object_cluster@assays[[ATAC_peak_assay]]@counts
       mat <- as.matrix(rowSums(as.matrix(mat)))
@@ -112,7 +113,7 @@ export_ATAC_scANANSE <- function(seurat_object,
       i = i + 1
     }else{print(paste0(cluster,' less than ',min_cells,' skipping'))}
   }
-  
+
   #ATAC peak matrix
   activity_matrix <- do.call(rbind, peak_count_lists)
   activity_matrix <- as.data.frame(activity_matrix)
@@ -125,7 +126,7 @@ export_ATAC_scANANSE <- function(seurat_object,
   rownames(activity_matrix) <- peaknames
   activity_matrix <- as.data.frame(activity_matrix)
   activity_matrix$average <- round(rowMeans(activity_matrix))
-  
+
   Peak_file <-paste(output_dir, "Peak_Counts.tsv", sep = '/')
   write.table(activity_matrix, Peak_file, sep = '\t', quote = F)
 }
@@ -133,23 +134,22 @@ export_ATAC_scANANSE <- function(seurat_object,
 #' config_scANANSE
 #'
 #' This functions generates a sample file and config file for running Anansnake based on the seurat object
-#' @param seurat object
-#' @param min_cells: minimum of cells a cluster needs to be exported
-#' @param output_dir: directory where the files are outputted
-#' @param genome: genomepy name or location of the genome fastq file
-#' @param cluster_id: ID used for finding clusters of cells
-#' @param additional_contrasts: additional contrasts to add between clusters within cluster_ID
+#' @param seurat_object seurat object
+#' @param min_cells minimum of cells a cluster needs to be exported
+#' @param output_dir directory where the files are outputted
+#' @param genome genomepy name or location of the genome fastq file
+#' @param cluster_id ID used for finding clusters of cells
+#' @param additional_contrasts additional contrasts to add between clusters within cluster_ID
 #' @export
 config_scANANSE <- function(seurat_object,
                                    min_cells = 50,
                                    output_dir = '~/',
                                    cluster_id = 'seurat_clusters',
                                    genome = './scANANSE/data/hg38',
-                                   additional_contrasts = 'None',
-                                   RNA_count_assay = "RNA"
+                                   additional_contrasts = 'None'
 ) {
   dir.create(file.path(output_dir), showWarnings = FALSE)
-  Idents(seurat_object) <- cluster_id 
+  Idents(seurat_object) <- cluster_id
   Peak_file <-paste(output_dir, "Peak_Counts.tsv", sep = '/')
   count_file <- paste(output_dir, "RNA_Counts.tsv", sep = '/')
   CPM_file <-paste(output_dir, "TPM.tsv", sep = '/')
@@ -172,7 +172,7 @@ config_scANANSE <- function(seurat_object,
   sample_file_df$assembly <- basename(genome)
   sample_file_df$anansesnake <- sample_file_df$sample
   sample_file_location <- paste0(output_dir,"/samplefile.tsv")
-  
+
   write.table(sample_file_df,sample_file_location,
               sep='\t',
               row.names = FALSE,
@@ -182,9 +182,9 @@ config_scANANSE <- function(seurat_object,
   if (type(additional_contrasts) == 'list'){print('adding additional contrasts')
     additional_contrasts <- paste0('anansesnake_',additional_contrasts)
     contrast_list = c(contrast_list,additional_contrasts)}
-  
+
   file=paste0(output_dir,"/config.yaml")
-  
+
   lines= c(
     paste0("result_dir: ",output_dir,'\n'),
     paste0("rna_samples: ",sample_file_location,'\n'),
@@ -200,10 +200,10 @@ config_scANANSE <- function(seurat_object,
     'plot_type: "png" \n',
     "get_orthologs: false \n",
     "contrasts: \n")
-  
+
   string= paste(lines, collapse = '')
   cat(string,file=paste0(output_dir,"/config.yaml"),append = F)
-  
+
   for (contr in contrast_list){
     cat(paste0('  - "',contr, '"'),"\n",file=paste0(output_dir,"/config.yaml"),append = T)
     print(contr)
@@ -213,11 +213,12 @@ config_scANANSE <- function(seurat_object,
 #' DEGS_scANANSE
 #'
 #' Calculate the differential genes needed for ananse influence
-#' @param seurat object
-#' @param min_cells: minimum of cells a cluster needs to be exported
-#' @param output_dir: directory where the files are outputted
-#' @param cluster_id: ID used for finding clusters of cells
-#' @param additional_contrasts: additional contrasts to add between clusters within cluster_ID
+#' @param seurat_object seurat object
+#' @param min_cells minimum of cells a cluster needs to be exported
+#' @param output_dir directory where the files are outputted
+#' @param cluster_id ID used for finding clusters of cells
+#' @param additional_contrasts additional contrasts to add between clusters within cluster_ID
+#' @importFrom Seurat Idents
 #' @export
 DEGS_scANANSE <- function(seurat_object,
                                       min_cells = 50,
@@ -238,32 +239,32 @@ DEGS_scANANSE <- function(seurat_object,
       cluster_names[i] <- cluster
       i = i+1 # Increase i to add clusters iteratively to the cluster_names list
     }}
-  
+
   #lets generate the snakemake config file
   contrast_list <- as.list(paste0('anansesnake_',cluster_names,'_average'))
   if (type(additional_contrasts) == 'list'){print('adding additional contrasts')
     additional_contrasts <- paste0('anansesnake_',additional_contrasts)
     contrast_list = c(contrast_list,additional_contrasts)}
-  
+
   for (contr in contrast_list){
     print(paste0('calculating DEGS for contrast ',contr))
     comparison1 <- str_split(contr, "_")[[1]][2]
     comparison2 <- str_split(contr, "_")[[1]][3]
-    
+
     DEG_file <- paste0(output_dir,'/deseq2/','hg38-anansesnake_',comparison1,'_', comparison2,'.diffexp.tsv')
-    
+
     if (file.exists(DEG_file)){print('skip')} else {
-      
+
       if (comparison2 == 'average'){
-        DEGS <- FindMarkers(seurat_object, 
+        DEGS <- FindMarkers(seurat_object,
                             ident.1 = comparison1,
-                            logfc.threshold = 0.1,  
-                            min.pct = 0.05, 
+                            logfc.threshold = 0.1,
+                            min.pct = 0.05,
                             return.thresh = 0.1)
-      }else{DEGS <- FindMarkers(seurat_object, 
-                                ident.1 = comparison1, 
-                                ident.2 = comparison2, 
-                                logfc.threshold = 0.1,  
+      }else{DEGS <- FindMarkers(seurat_object,
+                                ident.1 = comparison1,
+                                ident.2 = comparison2,
+                                logfc.threshold = 0.1,
                                 min.pct = 0.05,
                                 return.thresh = 0.1)}
       DEGS <- DEGS[c('avg_log2FC','p_val_adj')]
@@ -276,22 +277,23 @@ DEGS_scANANSE <- function(seurat_object,
 #' import_seurat_scANANSE
 #'
 #' import the influences from a anansnake directory into a seurat object
-#' @param seurat object
+#' @param seurat_object seurat object
+#' @param cluster_id ID used for finding clusters of cells
 #' @param anansnake_inf_dir influence directory generated by anansnake
 #' @export
 import_seurat_scANANSE <- function(seurat_object,
                                    cluster_id = 'seurat_clusters',
                                    anansnake_inf_dir = 'None'
 ){
-  
+
   files <- list.files(path=anansnake_inf_dir, pattern="*.tsv", full.names=TRUE, recursive=FALSE)
   GRN_files <- list.files(path=anansnake_inf_dir, pattern="_diffnetwork.tsv", full.names=TRUE, recursive=FALSE)
   files <- files[!files %in% GRN_files]
-  
+
   influence_scores_avg = list()
   influence_targets = list()
   i = 1
-  
+
   for (file in files){
     cell_target <- stringr::str_split(basename(file),"_")[[1]][2]
     cell_source <- stringr::str_split(basename(file),"_")[[1]][3]
@@ -305,12 +307,12 @@ import_seurat_scANANSE <- function(seurat_object,
       i = i + 1
     }
   }
-  
+
   avg_df <- influence_scores_avg %>% purrr::reduce(dplyr::full_join, by = "factor", copy = T)
   avg_df[is.na(avg_df)] = 0
   rownames(avg_df) <- avg_df$factor
   avg_df$factor <- NULL
-  
+
   #add the TF intensities to the seurat object
   TF_df <- t(avg_df)
   cell_signal_list <- list()
@@ -327,7 +329,7 @@ import_seurat_scANANSE <- function(seurat_object,
     i = i + 1
   }
   TF_array <- do.call("rbind", cell_signal_list)
-  
+
   #add cell IDs that do not have ANANSE signal and give them a NA vallue
   cell_barcodes_all <- rownames(as.data.frame(Idents(object = seurat_object)))
   cell_barcodes_missing <- cell_barcodes_all[!cell_barcodes_all %in% rownames(TF_array)]
@@ -337,9 +339,9 @@ import_seurat_scANANSE <- function(seurat_object,
   }
   rownames(cell_barcodes_missing) <- cell_barcodes_missing$cell_barcodes_missing
   cell_barcodes_missing$cell_barcodes_missing <- NULL
-  
+
   TF_output <- t(rbind(TF_array,cell_barcodes_missing))
-  
+
   seurat_object[['ANANSE']] <- TF_output %>% Seurat::CreateAssayObject(.)
   return(list(seurat_object,avg_df))
 }
